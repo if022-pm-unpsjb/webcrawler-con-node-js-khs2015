c47218f525fdf528c8d6aa0c7ee9e9800fe5ad6f
joaquin lima <limajoaquin@hotmail.com>
2018-08-15 14:33:53 -0300

    Versión 1 terminada

--- webcrawler-v1.js
+++ webcrawler-v1.js
@@ -1,13 +1,75 @@
-// para solicitudes HTTP
+/*
+ * Se concidera enlaces relativos a los que empiezan con "/""
+ * Se concidera enlaces absolutos a cualquiera que empiece con "http:"" o "https:"
+ */
+
+// modulo para hacer consultas HTTP (GET, POST, PUT)
 const request = require('request');
 
-// para poder usar funcionaes similares a JQuery
+// modulo para hacer busquedas en un documento HTML (similar a JQuery)
 const cheerio = require('cheerio');
 
-// la dirección donde se va a buscar
+// URL base en la cual se comienza a buscar enlaces relativos y absolutos
 //var baseURL = 'http://www.homeovet.com.ar/';
 var baseURL = 'https://www.google.com.ar/';
 //var baseURL = 'https://www.youtube.com/';
+
+function webCrawler(baseURL, deep) {
+	request(
+		baseURL, 
+		function (error, response, html) {
+			// imprimo la URL base
+			console.log("\nURL: " + baseURL);
+
+			// verifico que no haya errores y que la conexion a baseURL fue exitosa
+			if (!error && response.statusCode == 200) {
+				console.log("Conexión exitosa: HTTP Response " + response.statusCode);
+				// variable donde se va a almacenar el valor de un atributo href (el enlace)
+				var hrefValue = undefined;
+
+				// la URL relativa para cada elemento
+				var relativeURL;
+
+				// carga el html de la página devuelta en baseURL
+			    var $ = cheerio.load(html);
+			    
+			    // imprimo la cantidad de enlaces
+			    console.log("Cantidad de enlaces (absolutos y relativos): " + $('[href]').length);
+
+			    // seleeciona todos los elementos que tengan como atributo href (enlace)
+			    // e itero por cada elemento
+			    $('[href]').each(
+			    	function(i, element){
+
+						// obtengo el valor del atributo href, es decir, la URL
+						hrefValue = $(this).attr('href');
+
+						// verifico que el valor devuelto este definido
+						if (hrefValue != undefined) {
+
+							if (hrefValue.indexOf('/', 0) == 0) {
+								console.log("\t" + i + " - relativo: " + hrefValue);
+
+								// esto hay que probarlo, puede que no sea tan simple
+								relativeURL = baseURL + hrefValue.slice(1);
+								if (deep == true) {
+									webCrawler(relativeURL, false);
+								}
+
+							} else {
+								console.log("\t" + i + " - absoluto: " + hrefValue);
+							}
+						}
+			    	}
+			    );
+		  	} else {
+		  		console.log("Conexión fallida: HTTP Response " + response.statusCode);
+		  	}
+		}
+	);
+}
+
+webCrawler(baseURL, true);
 
 /*
 function callback_e(err, res, body) {
@@ -19,44 +81,39 @@ function callback_e(err, res, body) {
 request(baseURL, callback_e);
 */
 
+/*
 request(
 	baseURL, 
 	function (error, response, html) {
-		// verifico que no haya errores y la conexion fue exitosa
+		// verifico que no haya errores y que la conexion a baseURL fue exitosa
 		if (!error && response.statusCode == 200) {
-			// carga el html para poder usar jquery
+			//
+			var hrefValue = undefined;
+
+			// carga el html de la página devuelta en baseURL
 		    var $ = cheerio.load(html);
 		    
-		    // cantidad de elementos que tienen un atributo href (dirección)
-		    console.log("Cantidad de URLs: " + $('[href]').length);
-
-		    var pos_slash = baseURL.indexOf("//");
-
+		    // imprimo la URL base
 		    console.log("URL: " + baseURL);
-		    console.log("Posición de //: " + pos_slash);
+		    // imprimo la cantidad de enlaces
+		    console.log("Cantidad de enlaces: " + $('[href]').length);
 
-		    // seleeciona todos los elementos que tengan como atributo href
+		    // seleeciona todos los elementos que tengan como atributo href (enlace)
 		    // e itero por cada elemento
 		    $('[href]').each(
 		    	function(i, element){
-		    		//console.log(i);
-		    		//console.log(element);
-
-					/*
-					var a = $(this).prev();
-					if (a.attr("href") != undefined) {
-						//console.log(a.text());
-						console.log(a.attr("href"));	
-					}
-					*/
 
 					// obtengo el valor del atributo href, es decir, la URL
-					var hrefValue = $(this).attr('href');
+					hrefValue = $(this).attr('href');
 
 					// verifico que el valor devuelto este definido
 					if (hrefValue != undefined) {
 
-						// a continuación vamos a verificar que es una URL relativa
+						if (hrefValue.indexOf('/', 0) == 0) {
+							console.log(i + " - relativo: " + hrefValue);
+						} else {
+							console.log(i + " - absoluto: " + hrefValue);
+						}
 
 						// primero corrobora si la URL-base es parte de esta URL
 						if (hrefValue.indexOf(baseURL) != -1) {
@@ -71,7 +128,6 @@ request(
 
 							console.log(i, baseURL.concat(hrefValue));
 						}
-						
 					}
 					
 		    	}
@@ -82,7 +138,7 @@ request(
 	  	}
 	}
 );
-
+*/
 /*
 request('https://www.google.com.ar', (err, res, body) => {
 	        if (err) { return console.log(err); }
